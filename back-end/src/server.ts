bb4add780abc50d8004a22954e715f421b50b7b9
grace188031 <zabala_marygrace31@yahoo.com>
2025-04-15 22:18:47 -0400

    Adding comment POST configuration

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -3,9 +3,9 @@ const app = express();
 const port = 8000;
 
 const articleInfo = [
-  { articleName: 'learn-node', upvotes: 0,},
-  { articleName: 'learn-react', upvotes: 0},
-  { articleName: 'mongodb', upvotes: 0},
+  { articleName: 'learn-node', upvotes: 0, comments: [] },
+  { articleName: 'learn-react', upvotes: 0, comments: [] },
+  { articleName: 'mongodb', upvotes: 0,  comments: [] },
 ]
 
 app.use(express.json());
@@ -13,7 +13,17 @@ app.use(express.json());
 app.post('/api/articles/:articleName/upvote', (req, res) => {
   const article = articleInfo.find(a => a.articleName === req.params.articleName);
   article.upvotes += 1;
-  res.send(`The article with the name: ${req.params.articleName} now has ${article.upvotes} upvotes!`);
+  res.json(article);
+});
+
+app.post('/api/articles/:articleName/comments', (req, res) => {
+  const { articleName } = req.params;
+  const { postedBy, text } = req.body;
+  const article = articleInfo.find(a => a.articleName === articleName);
+  article.comments.push({ 
+    postedBy, 
+    text });
+  res.json(article);
 });
 
 // We can also use this syntax for upvote
