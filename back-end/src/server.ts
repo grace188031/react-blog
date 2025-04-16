31665a4314065635f3e9550c15d448aa63cbd230
grace188031 <zabala_marygrace31@yahoo.com>
2025-04-16 16:52:01 -0400

    AddingMongoDBExpress

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,12 +1,13 @@
 import express from 'express';
+import { MongoClient,ServerApiVersion } from 'mongodb';
 const app = express();
 const port = 8000;
 
-const articleInfo = [
-  { articleName: 'learn-node', upvotes: 0, comments: [] },
-  { articleName: 'learn-react', upvotes: 0, comments: [] },
-  { articleName: 'mongodb', upvotes: 0,  comments: [] },
-]
+// const articleInfo = [
+//   { articleName: 'learn-node', upvotes: 0, comments: [] },
+//   { articleName: 'learn-react', upvotes: 0, comments: [] },
+//   { articleName: 'mongodb', upvotes: 0,  comments: [] },
+// ]
 
 app.use(express.json());
 
@@ -33,6 +34,23 @@ app.post('/api/articles/:articleName/comments', (req, res) => {
 //   article.upvotes += 1;
 //   res.send(`The article with the name: ${articleName} now has ${article.upvotes} upvotes!`);
 // });
+
+app.get('/api/articles/:articleName', async (req, res) => {
+  const { articleName } = req.params;
+  const uri = 'mongodb://127.0.0.1:27017';
+  const client = new MongoClient(uri, {
+    serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+  });
+
+  await client.connect();
+  const db = client.db('full-stack-react-db');
+  const article = await db.collection('articles').findOne({ articleName } );
+  res.json(article);
+});
 
 
 app.listen(port, () => {
