5703c3e636f2c63925c1f03d6bc7b41d9882e64b
grace188031 <zabala_marygrace31@yahoo.com>
2025-04-16 23:08:51 -0400

    Importing useLoaderData hook and adding it in the html. COnfigured proxy in vite.config.ts

--- front-end/src/pages/ArticlePage.tsx
+++ front-end/src/pages/ArticlePage.tsx
@@ -1,8 +1,10 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useLoaderData } from 'react-router-dom';
 import articles from '../article-content';
 
 export default function ArticlePage() {
   const { name } = useParams();
+  const { upvotes, comments } = useLoaderData();
+  
   const article = articles.find(
     a => a.name === name
   );
@@ -11,6 +13,7 @@ export default function ArticlePage() {
     <>
     <div>
       <h1>{article.title}</h1>
+      <p>This article has Upvotes: {upvotes}</p>
       {article.content.map(p => <p key={p}>{p}</p>)}
     </div>
     </> 
--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  //add the following to solve the CORS issue
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true
+      }
+    }
+  }
 })
