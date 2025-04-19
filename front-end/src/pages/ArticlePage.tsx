4bb70a926a293506c4d95c73a54eecd229518c59
grace188031 <zabala_marygrace31@yahoo.com>
2025-04-19 00:15:07 -0400

    Added useState, updated function and button for upVote

--- front-end/src/pages/ArticlePage.tsx
+++ front-end/src/pages/ArticlePage.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useParams, useLoaderData } from 'react-router-dom';
 import articles from '../article-content';
 import CommentsList from '../CommentsList';
@@ -5,16 +6,25 @@ import axios from 'axios';
 
 export default function ArticlePage() {
   const { name } = useParams();
-  const { upvotes, comments } = useLoaderData();
-  
+  const { upvotes: initialUpvotes, comments } = useLoaderData();
+  const [upvotes, setUpvotes] = useState(initialUpvotes);
+
   const article = articles.find(
     a => a.name === name
   );
+
+  async function onUpvoteClicked() {
+    const response = await axios.post('/api/articles/' + name + '/upvote');
+    const updatedArticleData = response.data
+    setUpvotes(updatedArticleData.upvotes);
+  }
+
 
   return (
     <>
     <div>
       <h1>{article.title}</h1>
+      <button onClick={ onUpvoteClicked }>Upvote</button>
       <p>This article has Upvotes: {upvotes}</p>
       {article.content.map(p => <p key={p}>{p}</p>)}
       < CommentsList comments={comments} />
