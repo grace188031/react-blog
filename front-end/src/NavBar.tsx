484bf1627907d3cb82cd994a9d464be13bdedc33
grace188031 <zabala_marygrace31@yahoo.com>
2025-04-20 22:01:20 -0400

    Added logic for Login and Logout button

--- front-end/src/NavBar.tsx
+++ front-end/src/NavBar.tsx
@@ -1,7 +1,12 @@
 import { Link } from "react-router-dom";
-
+import { getAuth,signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 export default function NavBar() {
+  const isLoggedIn = false;
+  //const email = "grace@gmail.com";
+
+  const navigate = useNavigate();
   return (
     <nav>
       <ul>
@@ -13,6 +18,16 @@ export default function NavBar() {
         </li>
         <li>
             <Link to="/articleslist">Articles</Link>
+        </li>
+        {isLoggedIn && (
+          <li style={{ color: 'white'}}>
+            Logged in as {email}
+        </li>
+        )}
+        <li>
+            {isLoggedIn
+            ? <button onClick={() => signOut(getAuth())}>Sign Out</button>
+            : <button onClick={() => navigate('/login') }>Sign In</button>}
         </li>
       </ul>
     </nav>
