e91b96b918a2dfe74b7d2a6931275105979de9b7
grace188031 <zabala_marygrace31@yahoo.com>
2025-05-13 14:03:36 -0400

    Modified the post comment script returning json

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -111,21 +111,21 @@ app.post('/api/articles/:articleName/upvote', (req: Request, res: Response) => {
   })();
 });
 
-app.post('/api/articles/:articleName/comments', (req: Request, res: Response) => {
-  (async () => {
-    const { articleName } = req.params;
-    const { postedBy, text }: { postedBy: string; text: string } = req.body;
+app.post('/api/articles/:articleName/comments', async (req, res) => {
+  const { articleName } = req.params;
+  console.log(articleName)
+  const { postedBy, text } = req.body;
+  console.log(postedBy, text)
+  const newComment = { postedBy, text };
+  console.log(newComment)
+  const updatedComment = await db.collection('articles').findOneAndUpdate({ articleName }, {
+    $push: { comments: newComment as unknown as any} 
+  }, {
+    returnDocument: 'after',
+  });
+  res.json(updatedComment);
+});
 
-    const newComment = { postedBy, text };
-
-    const updatedArticle = await db.collection('articles').findOneAndUpdate(
-      { articleName },
-      {
-        $push: { comments: newComment as unknown as any },
-      },
-      { returnDocument: 'after' }
-    );
-  })});
   // Use environment variable PORT if available, otherwise default to 8000
   const PORT = process.env.PORT || 8000;
 
